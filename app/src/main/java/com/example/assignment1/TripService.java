472aea147b39c909e00bb8c63b9a222bbbf3a9d1
yuhsuan lee <yuhsuanlee94@gmail.com>
2025-04-11 00:16:04 -0400

    feat: Register and unregister broadcast receivers

--- app/src/main/java/com/example/assignment1/TripService.java
+++ app/src/main/java/com/example/assignment1/TripService.java
@@ -4,22 +4,29 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 public class TripService extends Service {
     private static final String TAG = TripService.class.getSimpleName();
     private static final String CHANNEL_ID = "TripNotificationChannel";
 
     private NotificationManager notificationManager;
+    private ConnectivityReceiver connectivityReceiver;
 
     @Override
     public void onCreate() {
         Log.i(TAG, "Trip Service Created");
+
         createNotificationChannel();
+
+        registerReceivers();
     }
 
     @Override
@@ -54,6 +61,31 @@ public class TripService extends Service {
             );
             channel.setDescription("Notifications about upcoming trips");
             notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    /**
+     * Registers all broadcast receivers used by the service
+     */
+    private void registerReceivers() {
+        // Connectivity
+        connectivityReceiver = new ConnectivityReceiver();
+        IntentFilter connFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.registerReceiver(this, connectivityReceiver, connFilter, ContextCompat.RECEIVER_EXPORTED);
+        } else {
+            registerReceiver(connectivityReceiver, connFilter);
+        }
+    }
+
+    /**
+     * Unregisters all broadcast receivers
+     */
+    private void unregisterReceivers() {
+        try {
+            unregisterReceiver(connectivityReceiver);
+        } catch (Exception e) {
+            Log.e(TAG, "Error unregistering receivers", e);
         }
     }
 }
